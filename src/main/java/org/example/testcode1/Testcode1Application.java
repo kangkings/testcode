c4a604a08b958b61862720347707ed6f1081d871
kangkigs <dlflq11@gmail.com>
2025-04-24 19:11:09 +0900

    [Init]

---
+++ src/main/java/org/example/testcode1/Testcode1Application.java
@@ -0,0 +1,13 @@
+package org.example.testcode1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testcode1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testcode1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/testcode1/Testcode1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testcode1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testcode1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
